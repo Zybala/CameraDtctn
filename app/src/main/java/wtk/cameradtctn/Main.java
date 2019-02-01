42740fa79a246a5bb080a0d094e3c6ef1fdc4da1
Zybala <Khunzybla@gmail.com>
2019-02-01 20:06:42 +0000

    OpenCV libs added, log

--- app/src/main/java/wtk/cameradtctn/Main.java
+++ app/src/main/java/wtk/cameradtctn/Main.java
@@ -2,9 +2,22 @@ package wtk.cameradtctn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class Main extends AppCompatActivity {
 
+    private static String TAG= "LogActivity";
+
+    static{
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OK");
+        }
+        else{
+            Log.d(TAG, "NOT OK");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
