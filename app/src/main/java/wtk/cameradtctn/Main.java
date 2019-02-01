6fdda31ab7b61283dc2b21a05921d3d34f8550ef
Zybala <Khunzybla@gmail.com>
2019-02-01 21:53:46 +0000

    cases loading image added

--- app/src/main/java/wtk/cameradtctn/Main.java
+++ app/src/main/java/wtk/cameradtctn/Main.java
@@ -3,24 +3,70 @@ package wtk.cameradtctn;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 
 public class Main extends AppCompatActivity {
 
     private static String TAG= "LogActivity";
+    JavaCameraView javaCameraView;
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case BaseLoaderCallback.SUCCESS: {
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
 
-    static{
-        if(OpenCVLoader.initDebug()){
-            Log.d(TAG, "OK");
         }
-        else{
-            Log.d(TAG, "NOT OK");
-        }
-    }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView!=null)
+            javaCameraView.disableView();
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView!=null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OK");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else{
+            Log.d(TAG, "NOT OK");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallBack);
+        }
     }
 }
